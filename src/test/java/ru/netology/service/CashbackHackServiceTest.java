d7bea3a61137b9ad426aa97a11c9b4cbce50f37e
Роман Журавлев <rzuravlev176@gmail.com>
2022-05-05 12:49:16 +0200

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual,expected);
+    }
+}
